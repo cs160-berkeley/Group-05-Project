9102ff9094aff0312ff709c2a03e29aa74d0ae15
Ali Nazzal <anazzal@berkeley.edu>
2016-11-06 15:30:21 -0800

    Added assets and lock/reheat page

--- capsulecompanionapp/main.js
+++ capsulecompanionapp/main.js
@@ -2,7 +2,7 @@
 
 import { // Imports for button behavior
     Button,
-    ButtonBehavior 
+    ButtonBehavior
 } from 'buttons';
 
 
@@ -14,7 +14,7 @@ let transparentSkin = new Skin({fill: "transparent"});
 // Container for the app header
 let appHeader = Container.template($ => ({
     left: 0, right: 0, top: 0, height: 40,
-    skin: new Skin({ fill : "#a181ef" }), style: smallBlack, 
+    skin: new Skin({ fill : "#a181ef" }), style: smallBlack,
     contents: [
         new Label({top:12, string: "capsule" }),
     ]
@@ -56,7 +56,7 @@ let pictureTemplate = Button.template($ => ({
     top: $.top, bottom: $.bottom, left: $.left, right: 10, skin: $.skin, name: $.name + "Button",
     contents: [
         new Picture({
-            width: $.width, 
+            width: $.width,
             url: "assets/" + $.name + $.imgExt,
         }),
         new Label({string: $.text, top: 30, style: new Style({ font: "13px", color: "black" })})
@@ -90,8 +90,123 @@ let homeScreen = Container.template($ => ({
     ],
 }));
 
-let splash = new splashScreen(); 
+let splash = new splashScreen();
 let home = new homeScreen();
 
-application.add(splash);
 
+// REHEAT PAGE
+
+var labelStyle = new Style( { font: "bold 25px", color:"black" } );
+var labelStyle2 = new Style( { font: "bold 20px", color:"black" } );
+var labelStyle3 = new Style( { font: "20px", color:"black" } );
+let orangeSkin = new Skin({ fill: 'white' });
+let bigText = new Style({ font: "bold 14px", color: "#333333" });
+
+var reheat = new Label({name: "reheat", left:0, right: 0, top:50, height:20, string:"Reheat Lasagna Container", style: labelStyle});
+
+var container = new Container({
+left: 0, right: 0, top: 80,height:2,
+skin: new Skin({ fill: "black" })
+});
+
+let kinomaLogo = new Picture({height: 90, url: "assets/lasagna.jpg"});
+kinomaLogo.coordinates = {height: 110, left: 0, right:0, top: 105, width: 100};
+
+var ready = new Label({name: "ready", left:0, right: 120, top:280, height:20, string:"Ready by:", style: labelStyle2});
+
+// var date = new Label({name: "date", left:0, right: 30, top:340, height:20, string:"Date:", style: labelStyle3});
+
+// Reheat button
+let MyButtonTemplate = Button.template($ => ({
+    top: 310, left: 0, right: 0,
+    contents: [
+        Label($, {left: 0, right: 0, height: 14, string: "Reheat", style: bigText})
+    ],
+    Behavior: class extends ButtonBehavior {
+        onTap(button){
+        }
+    }
+}));
+
+let ReheatPage = Container.template($ => ({
+    top: 0, bottom: 0, left: 0, right: 0,
+    skin: orangeSkin,
+    contents: [
+      reheat,
+      container,
+      kinomaLogo,
+      ready,
+      // new MyButtonTemplate()
+    ],
+}));
+
+
+
+
+
+
+
+
+// LOCK PAGE
+
+var labelStyle = new Style( { font: "bold 25px", color:"black" } );
+var labelStyle2 = new Style( { font: "bold 20px", color:"black" } );
+var labelStyle3 = new Style( { font: "20px", color:"black" } );
+let orangeSkin = new Skin({ fill: 'white' });
+let bigText = new Style({ font: "bold 14px", color: "#333333" });
+
+var lock = new Label({name: "lock", left:0, right: 0, top:50, height:20, string:"Lock My Avocado Container", style: labelStyle});
+
+var container = new Container({
+left: 0, right: 0, top: 80,height:2,
+skin: new Skin({ fill: "black" })
+});
+
+let kinomaLogo = new Picture({height: 90, url: "assets/avocado.jpg"});
+kinomaLogo.coordinates = {height: 110, left: 0, right:0, top: 105, width: 100};
+
+var until = new Label({name: "until", left:0, right: 120, top:280, height:20, string:"Lock until:", style: labelStyle2});
+
+// var date = new Label({name: "date", left:0, right: 30, top:340, height:20, string:"Date:", style: labelStyle3});
+
+// Reheat button
+let MyButtonTemplate = Button.template($ => ({
+    top: 310, left: 0, right: 0,
+    contents: [
+        Label($, {left: 0, right: 0, height: 14, string: "Lock", style: bigText})
+    ],
+    Behavior: class extends ButtonBehavior {
+        onTap(button){
+        }
+    }
+}));
+
+let LockPage = Container.template($ => ({
+    top: 0, bottom: 0, left: 0, right: 0,
+    skin: orangeSkin,
+    contents: [
+      lock,
+      container,
+      kinomaLogo,
+      until,
+      // new MyButtonTemplate()
+    ],
+}));
+
+
+
+
+
+
+
+// ADDS THE REHEAT PAGE
+// application.add(new ReheatPage());
+
+// ADDS THE LOCK PAGE
+// application.add(new LockPage());
+
+
+
+
+
+application.add(splash);
